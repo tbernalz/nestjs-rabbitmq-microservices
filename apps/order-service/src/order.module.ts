820ec9b9d4ba25837b1ea4b06df02f3a987417a3
Tomás Bernal <tbernalz@eafit.edu.co>
2025-04-29 00:47:14 -0500

    chore(infra): Configure RabbitMQ modules for services

--- apps/order-service/src/order.module.ts
+++ apps/order-service/src/order.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    RabbitMQModule.forRoot({
+      exchanges: [{ name: 'order_exchange', type: 'topic' }],
+      uri: process.env.RABBITMQ_URI || '',
+    }),
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })
--- apps/payment-service/src/payment.module.ts
+++ apps/payment-service/src/payment.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    RabbitMQModule.forRoot({
+      exchanges: [{ name: 'order_exchange', type: 'topic' }],
+      uri: process.env.RABBITMQ_URI || '',
+    }),
+  ],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
